c11bff340b1b8b41c2f956141686ea8825e78ddd
Sreelakshmi Anilkumar <81372148+Sreelakshmi393@users.noreply.github.com>
2023-12-04 14:13:19 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,14 @@
+a=int (input("enter side of sq :"))
+x=lambda a:a*a
+print(x(a))
+
+l=int (input("enter length of rect :"))	
+b=int (input("enter breadth of rect :"))
+x=lambda l,b:l*b
+print(x(l,b))
+
+h=int (input("enter height of triangle :"))
+b=int (input("enter base of triangle :"))
+x=lambda h,b:0.5*h*b
+print(x(h,b))
+
