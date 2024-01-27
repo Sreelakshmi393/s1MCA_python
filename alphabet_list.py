4964c71a9936c3f35e75e00e45a3f2203c6f1e5c
Sreelakshmi Anilkumar <81372148+Sreelakshmi393@users.noreply.github.com>
2024-01-27 10:37:32 +0530

    vowels from word list

---
+++ alphabet_list.py
@@ -0,0 +1,10 @@
+word = "dictionary"
+alphabet_list = []
+
+for i in word:
+    alphabet_list.append(i)
+
+for i in alphabet_list:
+    if(i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'):
+        print(i)
+
